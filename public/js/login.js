95d29cca2ac7dbbc58cd8e713e5022839ba370d4
Luca Amoriello <luca.amoriello@hotmail.it>
2020-11-16 19:22:49 +0100

    Login

---
+++ public/js/login.js
@@ -0,0 +1,3 @@
+document.getElementById('testLogin').addEventListener('click', function() {
+    doLogin()
+})
---
+++ public/js/userApi.js
@@ -0,0 +1,23 @@
+function doLogin() {
+    const xhr = new XMLHttpRequest()
+
+    var user = {
+        "email": "luca.amoriello@hotmail.it",
+        "password": "password"
+    }
+
+    xhr.addEventListener('readystatechange', function() {
+        if (this.readyState === this.DONE) {
+            return this.getResponseHeader("auth-token");
+        }
+    })
+
+    var apiv1 = "/api/v1";
+    var userApiV1 = apiv1 + '/users';
+
+    var loginEndpoint = location.protocol + "//" + location.host + userApiV1 + '/login';
+
+    xhr.open('POST', loginEndpoint);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(user));
+}
