0b71a704a13c2feafd37a63f440118705329109e
Luca Amoriello <luca.amoriello@hotmail.it>
2020-11-19 10:27:52 +0100

    Added new field to validation

--- match/matchValidation.js
+++ match/matchValidation.js
@@ -4,6 +4,7 @@ const Joi = require('joi');
 const matchValidation = (data) => {
     const schema = Joi.object({
         name: Joi.string().min(6).required(),
+        expectedPlayers: Joi.number().required(),
     });
     return schema.validate(data)
 }
