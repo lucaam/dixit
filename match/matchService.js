2e2636d2b1a83e318a034e188813dfaf80ad5126
Luca Amoriello <luca.amoriello@hotmail.it>
2020-11-19 10:26:51 +0100

    New crud

--- match/matchService.js
+++ match/matchService.js
@@ -14,8 +14,8 @@ function getMatchByName(name) {
     return matchRepository.getMatchByName(name);
 }
 
-function createMatch(name) {
-    return matchRepository.createMatch(name)
+function createMatch(name, expectedPlayers) {
+    return matchRepository.createMatch(name, expectedPlayers)
 }
 
 function deleteMatch(id) {
@@ -26,10 +26,42 @@ function addUserToMatch(name, user) {
     return matchRepository.addUserToMatch(name, user)
 }
 
-function setCards(name) {
-    const cards = cardService.getCards()   
+function incrementActualPlayers(name, number) {
+    return matchRepository.incrementActualPlayers(name, number)
+}
+
+function setNarrator(name, user) {
+    return matchRepository.setNarrator(name, user)
+}
+function setCards(name, cards) {
+    console.log("settingcards for match")
+    console.log(cards)
+    console.log(name)
     return matchRepository.setCards(name, cards)
 }
 
+function extractCards(match, number){
 
-module.exports = { getMatch, getMatches, createMatch, deleteMatch, getMatchByName, addUserToMatch, setCards}
+    var extractedCards = []
+    for(var i = 0; i < number; i++){
+        var randomNumber = Math.floor(Math.random() * (match.cards.length))
+        extractedCards.push(match.cards[randomNumber])
+        match.cards.splice(randomNumber, 1)
+    }
+
+    console.log(setCards(match.name, match.cards))
+    return extractedCards
+
+}
+
+function addCardOnTable(name, card) {
+    return matchRepository.addCardOnTable(name, card)
+    
+}
+
+function selectCardOnTable(name, card) {
+    return matchRepository.selectCardOnTable(name, card)
+    
+}
+
+module.exports = { getMatch, getMatches, createMatch, deleteMatch, getMatchByName, addUserToMatch, setCards, extractCards, incrementActualPlayers, setNarrator, addCardOnTable, selectCardOnTable}
