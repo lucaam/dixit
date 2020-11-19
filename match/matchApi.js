069c58cddd778f67ec020fb276f36b7ad68c1d72
Luca Amoriello <luca.amoriello@hotmail.it>
2020-11-19 10:25:38 +0100

    New APIs, save the user that created the match

--- match/matchApi.js
+++ match/matchApi.js
@@ -4,6 +4,7 @@ const matchService = require("./matchService");
 const { matchValidation } = require("./matchValidation");
 const verify = require("../user/verifyToken");
 const cardService = require("../card/cardService");
+const jwt = require('jsonwebtoken')
 
 
 // Create match
@@ -20,10 +21,15 @@ router.post("/", verify, async(request, response) => {
             .send("match with specified name already exists");
 
     try {
-        const savedMatch = await matchService.createMatch(request.body.name);
+        var savedMatch = await matchService.createMatch(request.body.name, request.body.expectedPlayers);
+        var decodeToken = jwt.decode(request.header('auth-token'))
+        var userAdded = matchService.addUserToMatch(savedMatch.name, decodeToken.user)
+
+        console.log("add user to match" + userAdded)
+        savedMatch.users.push(decodeToken.user)
         response.json(savedMatch);
     } catch (err) {
-        response.status(400).json({ message: err });
+        response.status(400).json({ message: err.message });
     }
 });
 
@@ -31,19 +37,22 @@ router.post("/", verify, async(request, response) => {
 router.get("/", verify, async(request, response) => {
     try {
         const matches = await matchService.getMatches();
+ 
         response.json(matches);
     } catch (err) {
-        response.json({ message: err });
+        response.status(400).json({ message: err.message });
     }
 });
 
 // Get match by name
 router.get("/:matchName", verify, async(request, response) => {
+
+
     try {
         const match = await matchService.getMatchByName(request.params.matchName);
         response.json(match);
     } catch (err) {
-        response.json({ message: err });
+        response.status(400).json({ message: err.message });
     }
 });
 
@@ -53,32 +62,51 @@ router.delete("/:matchId", verify, async(request, response) => {
         const removedmatch = await matchService.deleteMatch(request.params.matchId)
         response.json(removedmatch);
     } catch (err) {
-        response.json({ message: err });
+        response.status(400).json({ message: err.message });
     }
 });
 
 // Add new user to match's users list
-router.patch("/set/user:/matchName", verify, async(request, response) => {
+router.patch("/set/user/:matchName", verify, async(request, response) => {
+    console.log()
     try {
-        const updatedmatch = await matchService.addUserToMatch(
-            request.params.matchId,
-            request.body.user
+        var updatedMatch = await matchService.addUserToMatch(
+            request.params.matchName,
+            request.body
         );
-        response.json(updatedmatch);
+        response.json(updatedMatch);
     } catch (err) {
-        response.json({ message: err });
+        response.status(400).json({ message: err.message });
+    }
+});
+
+// Add new user to match's users list
+router.patch("/set/narrator/:matchName", verify, async(request, response) => {
+    console.log()
+    try {
+        var updatedMatch = await matchService.setNarrator(
+            request.params.matchName,
+            request.body
+        );
+        response.json(updatedMatch);
+    } catch (err) {
+        response.status(400).json({ message: err.message });
     }
 });
 
 // Add cards to a match
-router.patch("/set/cards/:/matchName", verify, async(request, response) => {
+router.patch("/set/cards/:matchName", verify, async(request, response) => {
+
+    const cards = await cardService.getCards()
+    
     try {
-        const updatedmatch = matchService.setCards(
-            request.params.matchName
+        const updatedmatch = await matchService.setCards(
+            request.params.matchName,
+            cards
         );
         response.json(updatedmatch);
     } catch (err) {
-        response.json({ message: err });
+        response.status(400).json({ message: err.message });
     }
 });
 
