8ae3e02f3efbfbaf6382e93ece7d4e03c27e5068
Luca Amoriello <luca.amoriello@hotmail.it>
2020-11-21 00:46:01 +0100

    Improved match validation

--- match/matchValidation.js
+++ match/matchValidation.js
@@ -4,9 +4,17 @@ const Joi = require('joi');
 const matchValidation = (data) => {
     const schema = Joi.object({
         name: Joi.string().min(6).required(),
-        expectedPlayers: Joi.number().required(),
+        expectedPlayers: Joi.number().integer().min(3).required(),
     });
     return schema.validate(data)
 }
 
-module.exports.matchValidation = matchValidation;
+const joinMatchValidation = (data) => {
+    const schema = Joi.object({
+        name: Joi.string().min(6).required()
+    });
+    return schema.validate(data)
+}
+
+module.exports = {matchValidation, joinMatchValidation};
+
