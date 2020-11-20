9273b01d51b91f94e86049e97463997501452c2e
Luca Amoriello <luca.amoriello@hotmail.it>
2020-11-21 00:46:11 +0100

    Minor changes

--- public/js/chat.js
+++ public/js/chat.js
@@ -54,46 +54,29 @@ const realMatch = {
     "lastAccess": "2020-11-17T17:46:02.161Z",
     "cards": [
         {
-            "_id": "5fb395c97037d40a14e663a2",
-            "name": "testCard",
-            "picture": "testCard.jpg",
-            "__v": 0
+            "_id": "5fb6985b9caa2e3b5a24124a",
+            "name": "TestCard1",
+            "picture": "test.png"
         },
         {
-            "_id": "5fb395d47037d40a14e663a3",
-            "name": "testCard1",
-            "picture": "testCard1.jpg",
-            "__v": 0
+            "_id": "5fb698649caa2e3b5a24124b",
+            "name": "TestCard2",
+            "picture": "test2.png"
         },
         {
-            "_id": "5fb395d87037d40a14e663a4",
-            "name": "testCard2",
-            "picture": "testCard2.jpg",
-            "__v": 0
+            "_id": "5fb6986e9caa2e3b5a24124c",
+            "name": "TestCard3",
+            "picture": "test3.png"
         },
         {
-            "_id": "5fb395dd7037d40a14e663a5",
-            "name": "testCard3",
-            "picture": "testCard3.jpg",
-            "__v": 0
+            "_id": "5fb698719caa2e3b5a24124d",
+            "name": "TestCard4",
+            "picture": "test4.png"
         },
         {
-            "_id": "5fb395e27037d40a14e663a6",
-            "name": "testCard4",
-            "picture": "testCard4.jpg",
-            "__v": 0
-        },
-        {
-            "_id": "5fb395e67037d40a14e663a7",
-            "name": "testCard5",
-            "picture": "testCard5.jpg",
-            "__v": 0
-        },
-        {
-            "_id": "5fb395eb7037d40a14e663a8",
-            "name": "testCard6",
-            "picture": "testCard6.jpg",
-            "__v": 0
+            "_id": "5fb698759caa2e3b5a24124e",
+            "name": "TestCard5",
+            "picture": "test5.png"
         }
     ],
     "users": [
@@ -111,7 +94,6 @@ const realMatch = {
             "registrationDate": "2020-11-16T11:07:22.894Z"
         }
     ],
-    "__v": 0,
     "narrator": {
         "role": "admin",
         "score": 0,
@@ -128,16 +110,15 @@ const realMatch = {
 }
 
 var card =   {
-    "_id": "5fb395c97037d40a14e663a2",
-    "name": "testCard",
-    "picture": "testCard.jpg"
+    "_id": "5fb6985b9caa2e3b5a24124a",
+    "name": "TestCard1",
+    "picture": "test.png"
 }
 
 var cardSelected =   {
-    "_id": "5fb395c97037d40a14e663a2",
-    "name": "testCard",
-    "picture": "testCard.jpg",
-    "selected": "0"
+    "_id": "5fb6985b9caa2e3b5a24124a",
+    "name": "TestCard1",
+    "picture": "test.png"
 }
 
 socket.on('hello', (data) => {
@@ -145,6 +126,7 @@ socket.on('hello', (data) => {
 })
 
 socket.on('newCardOnTable', (data) => {
+    console.log("new card on table ws")
     console.log(data)
 })
 
@@ -172,6 +154,7 @@ document.getElementById('hello').addEventListener('click', function() {
 })
 
 document.getElementById('addCardOnTable').addEventListener('click', function() {
+    console.log("addcardontablebutton")
     socket.emit('addCardOnTable', {card: card, match: realMatch});
 })
 
@@ -182,7 +165,7 @@ document.getElementById('ready').addEventListener('click', function() {
 
 document.getElementById('selectCard').addEventListener('click', function() {
     console.log(realMatch)
-    socket.emit('selectCard', {card: cardSelected, match: realMatch});
+    socket.emit('selectCard', {card: cardSelected, match: realMatch, user: fakeUser});
 })
 
 // adding click event on button
