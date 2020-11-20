8def027006d0b317e85ecf715ac7dcb848b6dd15
Luca Amoriello <luca.amoriello@hotmail.it>
2020-11-21 00:44:48 +0100

    Minor changes

--- match/matchSocketEvent.js
+++ match/matchSocketEvent.js
@@ -1,66 +1,69 @@
-const matchLogic = require('./matchSocketFunction')
-const Match = require('./matchModel')
+const matchLogic = require("./matchSocketFunction");
+const Match = require("./matchModel");
 function hello(socket) {
-
-    socket.on('hello', function() {
-        socket.emit('hello', { message: "Hello man!" })
-    })
-
-};
+  socket.on("hello", function () {
+    socket.emit("hello", { message: "Hello man!" });
+  });
+}
 
 function addCardOnTable(socket) {
-    socket.on('addCardOnTable', function(data) {
+  socket.on("addCardOnTable", function (data) {
+    // Update card on table
+    matchLogic.addCardOnTable(data.match.name, data.card);
 
-        // Update card on table
-        matchLogic.addCardOnTable(data.match.name, data.card)
-
-        // Tell everyone that a new card is available on the table
-        socket.to(data.match.name).emit('newCardOnTable', data.card)
-    })
+    // Tell everyone that a new card is available on the table
+    socket.to(data.match.name).emit("newCardOnTable", data.card);
+  });
 }
 
 function selectCard(socket) {
-    socket.on('selectCard', function(data) {
+  socket.on("selectCard", function (data) {
+    var cardSelected = data.card;
+    // Update card on table
+    matchLogic.selectCardOnTable(data.match.name, cardSelected, data.user);
 
-        var cardSelected = data.card
-        // Update card on table
-        matchLogic.selectCardOnTable(data.match.name, cardSelected)
+    // Tell everyone that a new card is available on the table
+    socket.to(data.match.name).emit("newCardSelected", cardSelected);
+    console.log("before evaluating turn end");
+    matchLogic
+      .endTurn(data.match.name)
+      .then(function (result) {
+        // Match can stop and we can evaluate who took how many points
 
-        // Tell everyone that a new card is available on the table
-        socket.to(data.match.name).emit('newCardSelected', cardSelected)
-        console.log("before evaluating turn end")
-        matchLogic.endTurn(data.match.name).then(function (result) {
-                console.log("Turn can end with new match data " + result)
-        }).catch((error) => console.log("should continue turn"))
-        
-        
-    
-    })
+        console.log("Turn can end with new match data " + result);
+      })
+      .catch((error) =>
+        // Match should continue
+        console.log("Turn shuld continue")
+      );
+  });
 }
 
 function readyToPlay(socket, io) {
-    // Data must contain the 2 objects: user, match
-    socket.on('readyToPlay', function(data) {
+  // Data must contain the 2 objects: user, match
+  socket.on("readyToPlay", function (data) {
+    console.log("readyToPlay");
+    // Tell everyone which user is ready
+    socket.to(data.match.name).emit("newUserReady", data.user);
 
-        // Tell everyone which user is ready
-        socket.to(data.match.name).emit('newUserReady', data.user)
-        
-        // Sending back to the user his object with cards assigned
-        var userWithCards = matchLogic.assignCards(data.user, data.match)
-        socket.to(data.match.name).emit('assignedCards', userWithCards)
+    // Sending back to the user his object with cards assigned
+    var userWithCards = matchLogic.assignCards(data.user, data.match);
+    socket.emit("assignedCards", userWithCards);
 
-        // match updated wit new player ready
-        matchLogic.incrementActualPlayers(data.match)
+    // AGGIORANRE IL MATCH NEL CLIENT
+    
+    // match updated wit new player ready
+    matchLogic.incrementActualPlayers(data.match);
 
-        // When expectedPlayers is equals to actualPlayers the match should start
-        if(matchLogic.readyToStart){
-            io.in(data.match.name).emit("readyToStart");
-
-        }
-
+    // When expectedPlayers is equals to actualPlayers the match should start
+    matchLogic
+      .readyToStart(data.match.name)
+      .then(function (res) {
+          console.log("prima di io.in")
+        io.in(data.match.name).emit("readyToStart", "Siamo pronti a giocare amico")
     })
+      .catch((error) => console.log("Not ready to play"));
+  });
 }
 
-
-
-module.exports = { hello, addCardOnTable, readyToPlay, selectCard}
+module.exports = { hello, addCardOnTable, readyToPlay, selectCard };
